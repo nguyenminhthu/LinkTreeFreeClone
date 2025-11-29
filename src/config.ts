fbe50c005241a5a1ce07b563c52d08f055469193
Minh Thu <thunm39@fpt.com>
2025-11-29 12:31:32 +0700

    Update info

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-export const SUBTITLE = "A free clone of LinkTree"
-export const TITLE = "LinkTreeFreeClone"
+export const SUBTITLE = "Tự động cho dân văn phong"
+export const TITLE = "AutoWork"
 export const TAGLINE = "Make your own LinkTree page for free"
 
 // Contacts
-export const contact = "Keep in touch"
+export const contact = "Giữ liên lạc`"
 export const phone = "+33600000000"
 export const email = "contact@yoandev.co"
